c27396c12eecad2f0c29db7d323442d88df1f2d5
Emil Rusev <emil.rusev@zigzag.global>
2025-05-17 12:47:42 +0300

    Connect with SQL server

--- ECommerce/ECommerce.Api/Program.cs
+++ ECommerce/ECommerce.Api/Program.cs
@@ -1,10 +1,25 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using ECommerce.Infrastructure;
+using Microsoft.EntityFrameworkCore;
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+var builder = WebApplication.CreateBuilder(args);
+
+// Infrastructure
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Application
+// builder.Services.AddScoped<IProductService, ProductService>(); // Example registration
+
+// Api
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    dbContext.Database.Migrate(); // Create database and apply migrations if necessary
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
---
+++ ECommerce/ECommerce.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿namespace ECommerce.Infrastructure;
+
+using ECommerce.Domain.Products;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    public DbSet<Product> Products => Set<Product>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>().ToTable("Products");
+        base.OnModelCreating(modelBuilder);
+    }
+}
